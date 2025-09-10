f7a5b0743fc8eb12e282bffd08643b8e0aa1f627
Jimmy Kurian <jimmykurian@microsoft.com>
2025-09-10 14:10:29 +0000

    chore(terrafomr): update versions #93647

--- guides/implement_ai_foundry_basic_with_azure_function_integration/terraform/ai_hub/versions.tf
+++ guides/implement_ai_foundry_basic_with_azure_function_integration/terraform/ai_hub/versions.tf
@@ -12,5 +12,9 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.5.0, < 4.0.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.0, < 4.0.0"
+    }
   }
 }
